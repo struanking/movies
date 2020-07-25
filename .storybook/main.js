a0c3a233532e7d54d77387b3129999a9b45ea97b
Struan King <struan.king@gmail.com>
2020-07-25 15:56:54 +0100

    Adds Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../**/*.stories.[tj]s'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-links/register',
+  ],
+};
