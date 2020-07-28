d3ec9f97958cdc3b2159f4e434e64484da3b7204
Struan King <struan.king@gmail.com>
2020-07-28 13:20:38 +0100

    Adds Cypress

---
+++ components/hidden-message.jsx
@@ -0,0 +1,11 @@
+export function HiddenMessage() {
+  return (
+    <div>
+      <label htmlFor="toggle">Show Message</label>
+      <input
+        id="toggle"
+        type="checkbox"
+      />
+    </div>
+  )
+}
